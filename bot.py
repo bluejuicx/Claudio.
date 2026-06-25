b155d4fae567b75a21dc9b8fd468e929cdec5adf
bluejuicx <mateomatiado@gmail.com>
2026-06-25 17:34:32 -0600

    Add basic Discord bot with hello command

--- bot.py
+++ bot.py
@@ -1,1 +1,18 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Conectado como {bot.user}")
+
+@bot.command()
+async def hola(ctx):
+    await ctx.send("¡Hola! Soy Claudio 🤖")
+
+bot.run(os.getenv("TOKEN"))
